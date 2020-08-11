13b0de7bbe625f5366a7d40bf44a2eea284dc49a
Ibrahima Ba <iboumocellou@yahoo.fr>
2020-08-11 04:12:22 +0200

    creation of the main file

---
+++ maze.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# -*- coding: Utf-8 -*
+
+"""
+MacGyver, maze game
+MacGyver must move through a labyrinth to collect items that will allow him to kill the guardian and
+finish the game.
+
+files : maze.py, classes_maze.py, constants_maze.py. README.md
+folder : ressource, .idea
+"""
+
+import random
+import pygame
+from pygame.locals import *
+
+from classes_maze import *
+from constants_maze import *
+
+pygame.init()
