b55980548cbd25629114286a957069b4d2737e03
Ibrahima Ba <iboumocellou@yahoo.fr>
2020-08-12 06:35:06 +0200

    modification of the classes

--- classes_maze.py
+++ classes_maze.py
@@ -6,11 +6,11 @@ from constants_maze import *
 
 
 class Start:
-	"""Class used to create the start of the game"""
-	def __init__(self, file):
-		self.file = file
-		self.structure = 0
+    """Class used to create the start of the game"""
 
+    def __init__(self, file):
+        self.file = file
+        self.structure = 0
 
     def generer(self):
         """Method for generating the start based on the file.
@@ -31,7 +31,6 @@ class Start:
                 structure_level.append(line_level)
             # We save this structure
             self.structure = structure_level
-
 
     def show(self, window):
         """Méthode permettant d'afficher le niveau en fonction
@@ -61,20 +60,20 @@ class Start:
 
 
 class Person:
-	"""Class used to create a character"""
-	def __init__(self, right, left, top, bottom, start):
-		#Character sprites
-		self.right = self.left = self.top = self.bottom = pygame.image.load(right).convert_alpha()
-		#Position of the character in squares and pixels
-		self.box_x = 0
-		self.box_y = 0
-		self.x = 0
-		self.y = 0
-		#Default direction
-		self.direction = self.right
-		#Start of the game
-		self.start = start
+    """Class used to create a character"""
 
+    def __init__(self, right, left, top, bottom, start):
+        # Character sprites
+        self.right = self.left = self.top = self.bottom = pygame.image.load(right).convert_alpha()
+        # Position of the character in squares and pixels
+        self.box_x = 0
+        self.box_y = 0
+        self.x = 0
+        self.y = 0
+        # Default direction
+        self.direction = self.right
+        # Start of the game
+        self.start = start
 
     def move(self, direction):
         """Method for moving the character"""
