f1a6058d7c23a95b4583e35d0797db862c9b42f9
Ibrahima Ba <iboumocellou@yahoo.fr>
2020-08-12 05:56:20 +0200

    creation of two news classes

--- classes_maze.py
+++ classes_maze.py
@@ -1,1 +1,28 @@
 """Class of the maze game"""
+
+import pygame
+from pygame.locals import *
+from constants_maze import *
+
+
+class Start:
+	"""Class used to create the start of the game"""
+	def __init__(self, file):
+		self.file = file
+		self.structure = 0
+
+
+class Person:
+	"""Class used to create a character"""
+	def __init__(self, right, left, top, bottom, start):
+		#Character sprites
+		self.right = self.left = self.top = self.bottom = pygame.image.load(right).convert_alpha()
+		#Position of the character in squares and pixels
+		self.box_x = 0
+		self.box_y = 0
+		self.x = 0
+		self.y = 0
+		#Default direction
+		self.direction = self.right
+		#Start of the game
+		self.start = start
