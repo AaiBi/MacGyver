fc478d4aca2643b3e1d8a00d5a7b21e5e32c57fb
Ibrahima Ba <iboumocellou@yahoo.fr>
2020-08-12 06:07:54 +0200

    creation of the home loop and and the code to launch the game

--- maze.py
+++ maze.py
@@ -40,3 +40,38 @@ while repeat:
 	# We reset these variables to 1 at each loop round
 	repeat_game = 1
 	repeat_home = 1
+
+	# Home loop
+	while repeat_home:
+
+		# Loop speed limitation
+		pygame.time.Clock().tick(30)
+
+		for event in pygame.event.get():
+
+			# If the user quits, we put the variables loop to 0 to cycle through none and close
+			if event.type == QUIT or event.type == KEYDOWN and event.key == K_ESCAPE:
+				repeat_home = 0
+				repeat_game = 0
+				repeat = 0
+				# Level choice variable
+				choice = 0
+
+			elif event.type == KEYDOWN:
+				# Launch of the game
+				if event.key == K_F1:
+					repeat_home = 0  # we leaves the home page
+					choice = 'maze_file'  # We launch the loading of maze_file which contains the labyrinth
+
+	# We check that the player has clicked on F1 for not charge the file if he leaves
+	if choice != 0:
+		# launch of the background
+		fond = pygame.image.load(background_image).convert()
+
+		# Generation of the maze_file, file containing the labyrinth
+		start = Start(choice)
+		start.generer()
+		start.show(window)
+
+		# Creation of the game
+		mg = Person(image_mac, image_mac, image_mac, image_mac, start)  # mg = MacGyver
