b51745d4fe4a919a724bf0d9fe5de24a253d169a
Ibrahima Ba <iboumocellou@yahoo.fr>
2020-08-12 04:38:26 +0200

    creation of the Start Class and the Person Class

--- maze.py
+++ maze.py
@@ -26,3 +26,26 @@ icon = pygame.image.load(icon_image)
 pygame.display.set_icon(icon)
 #Title
 pygame.display.set_caption(window_title)
+
+
+class Start:
+	"""Class used to create the start of the game"""
+	def __init__(self, file):
+		self.file = file
+		self.structure = 0
+
+
+class Person:
+	"""Class used to create a character"""
+	def __init__(self, right, left, top, bottom, start):
+		#Character sprites
+		self.right = self.left = self.top = self.bottom = pygame.image.load(right).convert_alpha()
+		#Position of the character in squares and pixels
+		self.box_x = 0
+		self.box_y = 0
+		self.x = 0
+		self.y = 0
+		#Default direction
+		self.direction = self.right
+		#Start of the game
+		self.start = start
