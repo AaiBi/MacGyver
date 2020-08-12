921d531e6491d830de3b4976831aa8d3b3ff4e15
Ibrahima Ba <iboumocellou@yahoo.fr>
2020-08-12 06:15:05 +0200

    creation of the funtions generer, show and move

--- classes_maze.py
+++ classes_maze.py
@@ -12,6 +12,54 @@ class Start:
 		self.structure = 0
 
 
+    def generer(self):
+        """Method for generating the start based on the file.
+        we create a general list, containing one list per line to display"""
+        # We open the file
+        with open(self.file, "r") as file:
+            structure_level = []
+            # We browse the lines of the file
+            for line in file:
+                line_level = []
+                # We browse the sprites (letters) contained in the file
+                for sprite in line:
+                    # We ignore the end of line "\ n"
+                    if sprite != '\n':
+                        # We add the sprite to the list of the line
+                        line_level.append(sprite)
+                # Add the line to the level list
+                structure_level.append(line_level)
+            # We save this structure
+            self.structure = structure_level
+
+
+    def show(self, window):
+        """Méthode permettant d'afficher le niveau en fonction
+        de la liste de structure renvoyée par generer()"""
+        # Chargement des images (seule celle d'arrivée contient de la transparence)
+        wall = pygame.image.load(wall_image).convert()
+        departure = pygame.image.load(departure_image).convert_alpha()
+        arrived = pygame.image.load(Gardien_image).convert_alpha()
+
+        # We go through the list of the level
+        number_line = 0
+        for line in self.structure:
+            # On parcourt les listes de lignes
+            num_case = 0
+            for sprite in line:
+                # We calculate the real position in pixels
+                x = num_case * sprite_size
+                y = number_line * sprite_size
+                if sprite == 'w':  # w = Wall
+                    window.blit(wall, (x, y))
+                elif sprite == 'd':  # d = Départ
+                    window.blit(departure, (x, y))
+                elif sprite == 'a':  # a = Arrivée
+                    window.blit(arrived, (x, y))
+                num_case += 1
+            number_line += 1
+
+
 class Person:
 	"""Class used to create a character"""
 	def __init__(self, right, left, top, bottom, start):
@@ -26,3 +74,44 @@ class Person:
 		self.direction = self.right
 		#Start of the game
 		self.start = start
+
+
+    def move(self, direction):
+        """Method for moving the character"""
+
+        # Moving to the right
+        if direction == 'right':
+            # Not to exceed the screen
+            if self.box_x < (number_sprites_side - 1):
+                # We check that the destination square is not a wall
+                if self.start.structure[self.box_y][self.box_x + 1] != 'w':
+                    # Moving a box
+                    self.box_x += 1
+                    # Calculation of the "real" position in pixels
+                    self.x = self.box_x * sprite_size
+            # main character image
+            self.direction = self.right
+
+        # Move to the left
+        if direction == 'left':
+            if self.box_x > 0:
+                if self.start.structure[self.box_y][self.box_x - 1] != 'w':
+                    self.box_x -= 1
+                    self.x = self.box_x * sprite_size
+            self.direction = self.left
+
+        # Moving up
+        if direction == 'top':
+            if self.box_y > 0:
+                if self.start.structure[self.box_y - 1][self.box_x] != 'w':
+                    self.box_y -= 1
+                    self.y = self.box_y * sprite_size
+            self.direction = self.top
+
+        # Moving down
+        if direction == 'bottom':
+            if self.box_y < (number_sprites_side - 1):
+                if self.start.structure[self.box_y + 1][self.box_x] != 'w':
+                    self.box_y += 1
+                    self.y = self.box_y * sprite_size
+            self.direction = self.bottom
