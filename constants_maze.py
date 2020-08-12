8a270c8328422d60aff17455368f686002c5cbf8
Ibrahima Ba <iboumocellou@yahoo.fr>
2020-08-12 06:23:08 +0200

    modifications of the directory

--- constants_maze.py
+++ constants_maze.py
@@ -9,9 +9,9 @@ window_side = number_sprites_side * sprite_size
 window_title = "MacGyver"
 
 #Listes des images du jeu
-image_mac = "images/MacGyver.png"
-home_image = "images/home.png"
-background_image = "images/background.jpg"
-wall_image = "images/wall.png"
-departure_image = "images/depart.png"
-Gardien_image = "images/Gardien.png"
+image_mac = "ressource/MacGyver.png"
+home_image = "ressource/home.png"
+background_image = "ressource/background.jpg"
+wall_image = "ressource/wall.png"
+departure_image = "ressource/depart.png"
+Gardien_image = "ressource/Gardien.png"
