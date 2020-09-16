f4c0be4e35cb1152b1f4c181ad1986c85d1d575a
Ibrahima Ba <iboumocellou@yahoo.fr>
2020-09-16 09:11:50 +0200

    Mise en majuscules des noms de variables

--- constants_maze.py
+++ constants_maze.py
@@ -1,21 +1,21 @@
 """Constants of the maze game"""
 
-#Paramètres de la fenêtre
-number_sprites_side = 15
-sprite_size = 30
-window_side = number_sprites_side * sprite_size
+# window settings
+NUMBER_SPRITES_SIDE = 15
+SPRITE_SIZE = 30
+WINDOW_SIZE = NUMBER_SPRITES_SIDE * SPRITE_SIZE
 
-#Personnalisation de la fenêtre
-window_title = "MacGyver"
+# window personalization
+WINDOW_TITLE = "MacGyver"
 
-#Listes des images du jeu
-image_mac = "ressource/MacGyver.png"
-home_image = "ressource/home.png"
-background_image = "ressource/background.jpg"
-wall_image = "ressource/wall.png"
-departure_image = "ressource/depart.png"
-Gardien_image = "ressource/Gardien.png"
-syringe_image = "ressource/seringue1.png"
-needle_image = "ressource/aiguille1.png"
-plastic_image = "ressource/tube_plastique1.png"
-ether_image = "ressource/ether1.png"
+# game image lists
+MACGYVER_IMAGE = "ressource/MacGyver.png"
+HOME_IMAGE = "ressource/home.png"
+BACKGROUND_IMAGE = "ressource/background.jpg"
+WALL_IMAGE = "ressource/wall.png"
+DEPARTURE_IMAGE = "ressource/depart.png"
+GARDIAN_IMAGE = "ressource/Gardien.png"
+SYRINGE_IMAGE = "ressource/seringue1.png"
+NEEDLE_IMAGE = "ressource/aiguille1.png"
+PLASTIC_IMAGE = "ressource/tube_plastique1.png"
+ETHER_IMAGE = "ressource/ether1.png"
